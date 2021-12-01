330eea41828bce3f6e7bdeac869d5ac530bf4dc9
Darpan Madlani <Darpan@proclink.com>
2021-12-01 08:39:46 +0530

    Select loop demo

--- autoGitPush.sh
+++ autoGitPush.sh
@@ -1,11 +1,9 @@
 echo '----------Git add -------------';
-
 git add .
 printf "\n";
 
 echo '----------Git commit ----------'
-
-git commit -m '$1';
+git commit -m "$1";
 printf "\n";
 
 echo '-----------Git push -----------'
